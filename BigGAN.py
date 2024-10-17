55bc8094db39ad4aad1a9def039763596f759dc6
GautamSharma11 <96952140+GautamSharma11@users.noreply.github.com>
2024-10-17 11:38:59 +0530

    Update BigGAN.py

--- BigGAN.py
+++ BigGAN.py
@@ -416,6 +416,7 @@ class G_D(nn.Module):
     # If training G, enable grad tape
     with torch.set_grad_enabled(train_G):
       # Get Generator output given noise
+      print(f"gy shape: {gy.shape}, dy shape: {dy.shape if dy is not None else None}")
       G_z = self.G(z, self.G.shared(gy))
       # Cast as necessary
       if self.G.fp16 and not self.D.fp16:
@@ -438,7 +439,6 @@ class G_D(nn.Module):
     # along the batch dimension for improved efficiency.
     else:
       D_input = torch.cat([G_z, x], 0) if x is not None else G_z
-      print(f"gy shape: {gy.shape}, dy shape: {dy.shape if dy is not None else None}")
       D_class = torch.cat([gy, dy], 0) if dy is not None else gy
       # Get Discriminator output
       D_out = self.D(D_input, D_class)
